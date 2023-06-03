435c80b9cf3de0dd3d4f455ac7128921b2b75be2
promise <pebuka351@gmail.com>
2023-06-03 16:00:04 +0100

    c

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -226,85 +226,89 @@ export default function Home() {
         />
       )}
       {loading && <Loading />}
-      {isLoggedIn && userDetails ? (
+      {isLoggedIn ? (
         <div className="w-full h-full grid grid-rows-3 grid-cols-1 gap-y-5">
-          <div className="w-full border border-solid border-[rgba(0,0,0,0.1)] rounded-lg bg-white box-border p-[15px]">
-            <p className="text-sm text-[rgba(0,0,0,0.9)] font-semibold">
-              Quick Access
-            </p>
+          {userDetails && (
+            <div className="w-full border border-solid border-[rgba(0,0,0,0.1)] rounded-lg bg-white box-border p-[15px]">
+              <p className="text-sm text-[rgba(0,0,0,0.9)] font-semibold">
+                Quick Access
+              </p>
 
-            <QuickAccess changed={changed} email={userDetails.email} />
-          </div>
-          <div className="bg-white w-full rounded-[6px] border border-solid border-[rgba(0,0,0,0.1)] row-span-2 pt-[14px]">
-            <div className="">
-              <div className="w-full px-[14px] flex justify-between items-center ">
-                <FilesNavator />
-                <div className="relative">
-                  <button
-                    onClick={() => setFileDropDown((prev) => !prev)}
-                    className="flex justify-center items-center border-none rounded text-white text-xs px-8 py-[10px] bg-[rgb(29,78,216)] hover:bg-[rgba(29,78,216,0.8)] gap-1"
-                  >
-                    <FontAwesomeIcon
-                      icon={faPlus}
-                      className="w-3 h-3 text-white"
+              <QuickAccess changed={changed} email={userDetails.email} />
+            </div>
+          )}
+          {userDetails && (
+            <div className="bg-white w-full rounded-[6px] border border-solid border-[rgba(0,0,0,0.1)] row-span-2 pt-[14px]">
+              <div className="">
+                <div className="w-full px-[14px] flex justify-between items-center ">
+                  <FilesNavator />
+                  <div className="relative">
+                    <button
+                      onClick={() => setFileDropDown((prev) => !prev)}
+                      className="flex justify-center items-center border-none rounded text-white text-xs px-8 py-[10px] bg-[rgb(29,78,216)] hover:bg-[rgba(29,78,216,0.8)] gap-1"
+                    >
+                      <FontAwesomeIcon
+                        icon={faPlus}
+                        className="w-3 h-3 text-white"
+                      />
+                      Add New
+                    </button>
+                    {fileDropDown && (
+                      <FileModal
+                        choosedFile={(id: string, name: string) => {
+                          setFileDropDown(false);
+                          addFile(id, name, pathname, userDetails.email).then(
+                            () => listDocuments(userDetails.email),
+                            (err) => console.log(err)
+                          );
+                        }}
+                        choosedFolder={() => {
+                          setFileDropDown(false);
+                          setFolderDropDown(true);
+                        }}
+                        loading={() => setLoading(true)}
+                      />
+                    )}
+                  </div>
+                </div>
+                <div className="pt-3 w-full flex pb-2 border-b border-solid border-[rgba(0,0,0,0.1)] px-[14px]">
+                  <p className="text-sm text-[rgba(0,0,0,0.6)]">Name</p>
+                </div>
+                <div
+                  id="fileView"
+                  className="w-full h-[37vh] overflow-y-scroll flex flex-col"
+                >
+                  {folders &&
+                    folders.map((item: any) => {
+                      return (
+                        <FolderList
+                          key={item.folder[0]}
+                          item={item}
+                          trash={(path: string, id: string) =>
+                            deleteFolder(path, id)
+                          }
+                        />
+                      );
+                    })}
+                  {files.map((item: any) => (
+                    <FileList
+                      item={item}
+                      key={item.id}
+                      trash={(id: string) => deleteFile(id)}
+                      clickedFavourite={() =>
+                        clickedFavourite(
+                          item.isFavourite,
+                          item.id,
+                          item.name,
+                          pathname
+                        )
+                      }
                     />
-                    Add New
-                  </button>
-                  {fileDropDown && (
-                    <FileModal
-                      choosedFile={(id: string, name: string) => {
-                        setFileDropDown(false);
-                        addFile(id, name, pathname, userDetails.email).then(
-                          () => listDocuments(userDetails.email),
-                          (err) => console.log(err)
-                        );
-                      }}
-                      choosedFolder={() => {
-                        setFileDropDown(false);
-                        setFolderDropDown(true);
-                      }}
-                      loading={() => setLoading(true)}
-                    />
-                  )}
+                  ))}
                 </div>
               </div>
-              <div className="pt-3 w-full flex pb-2 border-b border-solid border-[rgba(0,0,0,0.1)] px-[14px]">
-                <p className="text-sm text-[rgba(0,0,0,0.6)]">Name</p>
-              </div>
-              <div
-                id="fileView"
-                className="w-full h-[37vh] overflow-y-scroll flex flex-col"
-              >
-                {folders &&
-                  folders.map((item: any) => {
-                    return (
-                      <FolderList
-                        key={item.folder[0]}
-                        item={item}
-                        trash={(path: string, id: string) =>
-                          deleteFolder(path, id)
-                        }
-                      />
-                    );
-                  })}
-                {files.map((item: any) => (
-                  <FileList
-                    item={item}
-                    key={item.id}
-                    trash={(id: string) => deleteFile(id)}
-                    clickedFavourite={() =>
-                      clickedFavourite(
-                        item.isFavourite,
-                        item.id,
-                        item.name,
-                        pathname
-                      )
-                    }
-                  />
-                ))}
-              </div>
             </div>
-          </div>
+          )}
         </div>
       ) : (
         <Signin />
