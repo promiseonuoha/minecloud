de07eb7cd8559274aabbce6f70a131ad96f591f1
promise <pebuka351@gmail.com>
2023-06-09 20:02:58 +0100

    Link to Appwrite

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,16 @@
 export default function Contact() {
   return (
-    <div className="w-full h-auto border border-solid border-black/10 rounded-lg bg-white box-border p-4">
+    <div className="w-full h-auto p-4">
+      <p className="text-xs pb-3 ">#Appwrite Hackathon 2023</p>
       <p className="text-xs font-medium text-black/80">
-        Minecloud is a Cloud storage service that provide a convenient and
-        secure way to store files and folders remotely, allowing you to access
-        them from any device with an internet connection. It offer a virtual
-        storage space where you can upload, store, and organize your digital
-        files, eliminating the need for physical storage devices like hard
-        drives or USB flash drives. One of the key advantages of cloud storage
-        is its accessibility. By uploading your files to the cloud, you can
-        easily retrieve them from anywhere, whether you are using a computer,
-        smartphone, or tablet. This flexibility is particularly useful for
-        individuals who frequently work on different devices or need to access
-        their files on the go.
+        Powered By {""}
+        <a className="underline" href="https://appwrite.io">
+          AppWrite
+        </a>{" "}
+        &{" "}
+        <a className="underline" href="https://vercel.com">
+          Vercel
+        </a>
       </p>
     </div>
   );
