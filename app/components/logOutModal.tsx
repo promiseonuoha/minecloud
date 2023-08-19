004ec43a244f70f4822c6ede6190ac152e7e3ac0
Promise <pebuka351@gmail.com>
2023-08-19 23:30:04 +0100

    Update logOutModal.tsx

--- app/components/logOutModal.tsx
+++ app/components/logOutModal.tsx
@@ -2,20 +2,16 @@
 import { useState, useEffect } from "react";
 
 export default function LogOutModal({ cancel, logout }: any) {
-  const [transform, setTransform] = useState("translateY(-8px)");
   const [opacity, setOpacity] = useState(0);
 
   useEffect(() => {
     setOpacity(1);
-    setTimeout(() => {
-      setTransform("translateY(0px)");
-    }, 300);
   }, []);
 
   return (
     <div className="absolute w-screen h-screen left-0 top-0 bg-black/10 z-50 flex justify-center items-center">
       <div
-        style={{ transform: transform, opacity: opacity }}
+        style={{ opacity: opacity }}
         className="flex flex-col duration-700 rounded bg-white w-[250px] p-3"
       >
         <p className="pb-1 text-sm font-medium text-black/80">Are you sure</p>
@@ -25,13 +21,13 @@ export default function LogOutModal({ cancel, logout }: any) {
         <div className=" flex justify-between w-full items-center">
           <button
             onClick={cancel}
-            className="w-fourtyEightPercent py-2 border border-solid hover:scale-105 border-black/10 text-black/80 text-xs rounded"
+            className="w-fourtyEightPercent py-2.5 border border-solid hover:scale-105 border-black/10 text-black/80 text-xs rounded"
           >
             Cancel
           </button>
           <button
             onClick={logout}
-            className="w-fourtyEightPercent py-2 bg-blue-700 hover:scale-105 border-none text-white text-xs rounded"
+            className="w-fourtyEightPercent py-2.5 bg-blue-700 hover:scale-105 border-none text-white text-xs rounded"
           >
             LogOut
           </button>
